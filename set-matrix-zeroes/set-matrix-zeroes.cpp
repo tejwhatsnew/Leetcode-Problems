37446c8d195d28135409bfb17bcbb32d7d8d8ae4
Tejas Padhiyar <78201942+tejwhatsnew@users.noreply.github.com>
2023-07-08 01:02:36 +0530

    Time: 12 ms (91.99%) | Memory: 13.5 MB (14.75%) - LeetSync

---
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> row(m);
+        vector<int> col(n);
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        } 
+
+        for(int r = 0; r < row.size(); r++)
+        {
+            for(int c = 0; c < col.size(); c++)
+            {
+                if(row[r]==1 or col[c]==1)
+                    matrix[r][c]=0;
+            }    
+        }
+    }
+};
