dc26a6d4b0352d81e8118cdca75ac06fd87f0472
Tejas Padhiyar <78201942+tejwhatsnew@users.noreply.github.com>
2023-07-13 16:48:59 +0530

    Time: 49 ms (29.42%) | Memory: 18.9 MB (74.01%) - LeetSync

---
+++ merge-intervals/merge-intervals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        for(int i = 0; i < n; i++)
+        {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+            if(!res.empty() && end <= res.back()[1])
+                continue;
+            
+            for(int j =i + 1; j < n; j++)
+            {
+                if(intervals[j][0] <= end)
+                {
+                    end = max(end, intervals[j][1]);
+                }
+                else
+                    break;
+            }
+            res.push_back({start, end});
+        }
+
+        return res;
+    }
+};
