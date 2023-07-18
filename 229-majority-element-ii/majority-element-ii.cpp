3d8297463e06b71a220d8ce09aae217d61b3e036
Tejas Padhiyar <78201942+tejwhatsnew@users.noreply.github.com>
2023-07-19 05:04:59 +0530

    Time: 24 ms (7.26%) | Memory: 15.8 MB (64.72%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,19 +2,46 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> list;
-        map<int,int> hash;
-        int mini = int(nums.size()/3) + 1;
-        
+        int cnt1 = 0, cnt2 = 0, ele1, ele2;
         for(int i = 0; i < nums.size(); i++)
         {
-            hash[nums[i]]++;
-
-            if(hash[nums[i]] == mini)
-                list.push_back(nums[i]);
-            
-            if(list.size() == 2) break;
+            if(cnt1 == 0 && ele2 != nums[i])
+            {
+                cnt1 = 1;
+                ele1 = nums[i];
+            }
+            else if(cnt2 == 0 && ele1 != nums[i])
+            {
+                cnt2 = 1;
+                ele2 = nums[i];
+            }
+            else if(nums[i] == ele1) cnt1++;
+            else if(nums[i] == ele2) cnt2++;
+            else
+            {
+                cnt1--;
+                cnt2--;
+            }
         }
+
+        cnt1 = 0;
+        cnt2 = 0;
+
+        int mini = int(nums.size()/3) + 1;
+
+        vector<int> list;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] == ele1) cnt1++;
+            if(nums[i] == ele2) cnt2++;  
+        }
+
+        if(cnt1 >= mini) list.push_back(ele1);
+        if(cnt2 >= mini) list.push_back(ele2);
+
+        sort(list.begin(), list.end());
+        
         return list;
     }
 };
