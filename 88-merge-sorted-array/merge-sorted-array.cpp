91d46cd113cd2e854c9a4b8bc6a2433b0a3b8e2b
Tejas Padhiyar <78201942+tejwhatsnew@users.noreply.github.com>
2023-07-18 19:13:48 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (39.66%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // int left = nums1.size() - 1, right = 0;
+        // while(left >= 0 and right < nums2.size())
+        // {
+        //         if(nums1[left] > nums2[right])
+        //         {
+        //             swap(nums1[left], nums2[right]);
+        //             left--, right++;
+        //         }
+        //         else
+        //             break;
+            
+        // }
+
+        int i = 0;
+
+        while(i < nums2.size())
+        {
+            nums1[m + i] = nums2[i];
+            i++;
+        }
+        // cout<<nums1.size();
+        sort(nums1.begin(), nums1.end());
+        // sort(nums2.begin(), nums2.end());
+    }
+};
