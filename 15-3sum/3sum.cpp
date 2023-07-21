bd4d1e58c099058480052a72c8176d2d47c86ab4
Tejas Padhiyar <78201942+tejwhatsnew@users.noreply.github.com>
2023-07-22 02:09:24 +0530

    Time: 133 ms (91.43%) | Memory: 23.9 MB (86.43%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -35,40 +35,5 @@ public:
             
         }   
         return ans;
-
-        // vector<vector<int>> triplets;
-        // sort(nums.begin(), nums.end());
-
-        // for (int i = 0; i < nums.size() - 2; i++) {
-        //     if (i > 0 && nums[i] == nums[i - 1])
-        //         continue;
-
-        //     int left = i + 1;
-        //     int right = nums.size() - 1;
-
-        //     while (left < right) {
-        //         int total_sum = nums[i] + nums[left] + nums[right];
-
-        //         if (total_sum < 0) {
-        //             left++;
-        //         } else if (total_sum > 0) {
-        //             right--;
-        //         } else {
-        //             triplets.push_back({nums[i], nums[left], nums[right]});
-
-        //             // Skip duplicate elements
-        //             while (left < right && nums[left] == nums[left + 1])
-        //                 left++;
-        //             while (left < right && nums[right] == nums[right - 1])
-        //                 right--;
-
-        //             left++;
-        //             right--;
-        //         }
-        //     }
-        // }
-
-        // return triplets;
-
     }
 };
