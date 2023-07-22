fab8bb15f5959127da463dd41cccd702d7976230
Tejas Padhiyar <78201942+tejwhatsnew@users.noreply.github.com>
2023-07-22 23:21:24 +0530

    Time: 102 ms (59.53%) | Memory: 13.2 MB (72.56%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,31 +1,73 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        set<vector<int>> pairs;
-        int n = nums.size();
+        // vector<vector<int>> ans;
+        // int n = nums.size();
+        // sort(nums.begin(), nums.end());
+        // for(int i = 0; i < n - 3; i++)
+        // {
+        //     if (i > 0 && nums[i] == nums[i - 1]) continue;
+        //     for(int j = i+1; j < n - 2; j++)
+        //     {
+        //         if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+        //         int k = j + 1;
+        //         int l = n - 1;
+        //         while(k < l)
+        //         {
+        //             long long sum = nums[i] + nums[j];
+        //             sum += nums[k];
+        //             sum += nums[l];
+        //             if(sum < target) k++;
+        //             else if(sum > target) l--;
+        //             else
+        //             {
+        //                 ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+        //                 while(k < l && nums[k] == nums[k-1]) k++;
+        //                 while(k < l && nums[l] == nums[l+1]) l++;
+        //                 k++;
+        //                 l++;
+        //             }
+        //         }
+        //     }
+        // }
+        // return ans;
+        int n = nums.size(); //size of the array
+    vector<vector<int>> ans;
 
-        for(int  i = 0; i < n - 2; i++)
-        {
-            for(int j = i+1; j < n - 1; j++)
-            {
-                set<long long> hashset;
-                for(int k = j+1; k < n; k++)
-                {
-                    long long sum = nums[i] + nums[j];
-                    sum += nums[k];
-                    long long fourth = target - (sum);
-                    if(hashset.find(fourth) != hashset.end())
-                    {
-                        vector<int> temp = {nums[i], nums[j], nums[k], (int)fourth};
-                        sort(temp.begin(), temp.end());
-                        pairs.insert(temp);
-                    }
-                    hashset.insert(nums[k]);
+    // sort the given array:
+    sort(nums.begin(), nums.end());
+
+    //calculating the quadruplets:
+    for (int i = 0; i < n; i++) {
+        // avoid the duplicates while moving i:
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int j = i + 1; j < n; j++) {
+            // avoid the duplicates while moving j:
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+            // 2 pointers:
+            int k = j + 1;
+            int l = n - 1;
+            while (k < l) {
+                long long sum = nums[i];
+                sum += nums[j];
+                sum += nums[k];
+                sum += nums[l];
+                if (sum == target) {
+                    vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                    ans.push_back(temp);
+                    k++; l--;
+
+                    //skip the duplicates:
+                    while (k < l && nums[k] == nums[k - 1]) k++;
+                    while (k < l && nums[l] == nums[l + 1]) l--;
                 }
+                else if (sum < target) k++;
+                else l--;
             }
         }
+    }
 
-        vector<vector<int>> ans(pairs.begin(), pairs.end());
-        return ans;
+    return ans;
     }
 };
