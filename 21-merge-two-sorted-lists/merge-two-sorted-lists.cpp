cfd51ccce3513ef4c81f905baf4befd2201a4874
Tejas Padhiyar <78201942+tejwhatsnew@users.noreply.github.com>
2023-07-27 22:03:13 +0530

    Time: 7 ms (75.55%) | Memory: 14.8 MB (53.74%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -13,48 +13,24 @@ public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if(!list1) return list2;
         if(!list2) return list1;
-        ListNode *list3 = NULL;
-        ListNode* dummy = NULL;
-        while(list1 && list2)
+        
+        if(list1->val > list2->val) swap(list1,list2);
+
+        ListNode *res = list1;
+
+        while(list1 != NULL && list2 != NULL)
         {
-            if(list1->val <= list2->val)
+            ListNode* temp = list1;
+            
+            while(list1 != NULL && list1->val <= list2->val)
             {
-                if(!list3)
-                {
-                    list3 = list1;
-                    dummy = list3; 
-                }
-                else
-                {
-                    dummy->next = list1;
-                    dummy = dummy->next;
-                }
+                temp = list1;
                 list1 = list1->next;
             }
-            else
-            {
-                if(!list3)
-                {
-                    list3 = list2;
-                    dummy = list3; 
-                }
-                else
-                {
-                    dummy->next = list2;
-                    dummy = dummy->next;
-                }
-                list2 = list2->next;
-            }
+
+            temp->next = list2;
+            swap(list1,list2);
         }
-        if(list1)
-        {
-            dummy->next = list1;
-           
-        }
-        else
-        {
-            dummy->next = list2;
-        }
-        return list3;
+        return res;
     }
 };
