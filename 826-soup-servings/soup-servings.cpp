996e4d3b62f0f7c06d0b1dbd94a572cb4dd04157
Tejas Padhiyar <78201942+tejwhatsnew@users.noreply.github.com>
2023-07-29 23:50:54 +0530

    Time: 32 ms (30.04%) | Memory: 391.1 MB (5.32%) - LeetSync

---
+++ 826-soup-servings/soup-servings.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<pair<int,int>> serves = {{100,0}, {75,25}, {50,50}, {25,75}};
+    vector<vector<double>> dummy;
+    double solve(double A, double B)
+    {
+        if(A <= 0 && B<= 0)
+            return 0.5;
+        
+        if(A <= 0)
+            return 1;
+        
+        if(B <= 0)
+            return 0;
+        
+        if(dummy[A][B] != -1.0)
+            return dummy[A][B];
+        
+        double pro = 0.0;
+
+        for(auto p : serves)
+        {
+            int A_token = p.first;
+            int B_token = p.second;
+
+            pro += solve(A - A_token, B - B_token);
+        }
+
+        return dummy[A][B] = 0.25 * pro;
+    }
+    double soupServings(int n) {
+
+        if(n >= 6000)
+            return 1;
+
+        dummy.resize(n+1, vector<double>(n+1, -1.0));
+        return solve(n, n);
+    }
+};
