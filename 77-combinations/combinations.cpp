01043f4bf9d445cc13f3696f276c32a4ecca9717
Tejas Padhiyar <78201942+tejwhatsnew@users.noreply.github.com>
2023-08-01 17:49:42 +0530

    Time: 10 ms (93.32%) | Memory: 9 MB (83.81%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void backtrack(std::vector<std::vector<int>>& combinations, std::vector<int>& current_combination, int start, int n, int k) {
+        if (k == 0) {
+            combinations.push_back(current_combination);
+            return;
+        }
+
+        for (int i = start; i <= n; ++i) {
+            current_combination.push_back(i);
+            backtrack(combinations, current_combination, i + 1, n, k - 1);
+            current_combination.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> com;
+
+         backtrack(res, com, 1, n, k);
+
+        return res;
+    }
+};
