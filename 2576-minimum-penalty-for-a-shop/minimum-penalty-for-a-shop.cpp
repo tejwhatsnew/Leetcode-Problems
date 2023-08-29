ff8f74b95ecc13c52d4ba4d39be2fd84fbe2cb1f
Tejas Padhiyar <78201942+tejwhatsnew@users.noreply.github.com>
2023-08-30 01:51:24 +0530

    Time: 21 ms (88.07%) | Memory: 10.7 MB (72.63%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int s = customers.size();
-        vector<int> y,n;
-        y.push_back(0);
-        n.push_back(0);
-        int count = 0;
+        int hour = 0, count = 0;
 
-        for(int i = 0; i < s; i++)
+        for(int i = 0; i < customers.length(); i++)
         {
-            if(customers[i] == 'N') count++;
-            n.push_back(count);
-        }
-
-        count = 0;
-
-        for(int i = s-1; i >= 0; i--)
-        {
-            if(customers[i] == 'Y') count++;
-            y.push_back(count);
-        }
-        reverse(y.begin(), y.end());
-        
-        int res = INT_MAX, hour = 0;
-        for(int i = 0; i < s + 1; i++)
-        {
-            int h = y[i] + n[i];
-            if(h < res)
+            if(customers[i] == 'Y')
             {
-                res = h;
-                hour = i;
+                count++;
+
+                if(count > 0)
+                {
+                    hour = i + 1;
+                    count = 0;
+                }
             }
+            else
+                count--;
         }
 
         return hour;
