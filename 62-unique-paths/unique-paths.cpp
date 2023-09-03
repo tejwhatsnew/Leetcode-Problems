a723af5be77114da3c3fe1a26467c0f0b14fa5ff
Tejas Padhiyar <78201942+tejwhatsnew@users.noreply.github.com>
2023-09-04 00:16:20 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (78.55%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         double res = 1;
-        int N = m + n -2;
-        int r = m -1;
+        int N = m + n - 2;
+        int r = m - 1;
 
         for(int i = 1; i <= r; i++)
         {
