8e3d78257eb158cb79a9b30365de2649fb54b63f
Tejas Padhiyar <78201942+tejwhatsnew@users.noreply.github.com>
2023-09-10 01:06:34 +0530

    Time: 4 ms (27.62%) | Memory: 6.7 MB (46.62%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,22 +1,33 @@
 class Solution {
-private:
-    int solve(vector<int>& nums, int target,vector<int>& dp)
-    {
-        if(target < 0) return 0;
-        if(target == 0) return 1;
-        if(dp[target] != -1) return dp[target];
+// private:
+//     int solve(vector<int>& nums, int target,vector<int>& dp)
+//     {
+//         if(target < 0) return 0;
+//         if(target == 0) return 1;
+//         if(dp[target] != -1) return dp[target];
 
-        int ans = 0;
-        for(int i = 0; i < nums.size(); i++)
-        {
-            ans += solve(nums, target-nums[i], dp);
-        }
+//         int ans = 0;
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             ans += solve(nums, target-nums[i], dp);
+//         }
 
-        return dp[target] = ans;
-    }
+//         return dp[target] = ans;
+//     }
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target+1, -1);
-        return solve(nums,target,dp);   
+        vector<unsigned long long> dp(target+1, 0);
+        dp[0] = 1;
+        
+        for(int i = 1; i <= target; i++)
+        {
+            for(int j = 0; j < nums.size(); j++)
+            {
+                int ans = nums[j];
+                if(i - ans >= 0)
+                    dp[i] += dp[i - ans];
+            }
+        }
+        return dp[target];   
     }
 };
